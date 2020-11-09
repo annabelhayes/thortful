ea31d85816d27b01381be40ca8469c865cdf0dd1
annabelhayes <annabel.hayes@googlemail.com>
2020-11-09 13:37:20 +0000

    feat(nav): create nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
