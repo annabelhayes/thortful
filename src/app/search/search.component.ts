9930dbf7d2d8decd8697318973eb8e2141a4c7b8
annabelhayes <annabel.hayes@googlemail.com>
2020-11-11 11:48:01 +0000

    feat(search): enable joke selection

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { DadJoke } from '../dadjoke';
 import { DadJokeService } from '../dadjoke.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -12,10 +13,14 @@ export class SearchComponent implements OnInit {
   faPaperPlane = faPaperPlane;
   searchInputText: string;
   jokes: DadJoke[];
-  constructor(private dadJokeService: DadJokeService) { }
+  constructor(private dadJokeService: DadJokeService, public router: Router) { }
 
   ngOnInit() {
     this.dadJokeService.searchJokes().subscribe(jokes => (this.jokes = jokes));
+  }
+
+  navigateToJoke(id) {
+    this.router.navigate([`home/${id}`]);
   }
 
   emitSearch(term) {
